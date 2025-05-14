fd810fd4fda615f782d85339f38b2604ffc7daeb
Murriel <murriel@google.com>
2025-05-13 17:31:12 -0700

    updating with variable options

--- terraform-gce/main.tf
+++ terraform-gce/main.tf
@@ -1,8 +1,12 @@
+# main.tf 
+# This file defines the main infrastructure for a Google Cloud project, including network, compute instances, and firewalls.
+# Different options for hardcoded values and variables are provided below (example: project)
 
 module "project_services" {
   source  = "terraform-google-modules/project-factory/google//modules/project_services"
   version = "~> 18.0"
-  project_id = var.project_id
+  project_id = "<your project here>"
+  # project_id = var.project_id
   disable_services_on_destroy = false
   disable_dependent_services  = false
 
@@ -18,23 +22,26 @@ module "project_services" {
 }
 
 resource "google_compute_network" "my_vpc" {
+  name    = "my-vpc"
   project = "<your project here>"
-  name = "my-vpc"
+  # project = var.project_id
   depends_on = [module.project_services]
   auto_create_subnetworks = false # This creates subnets in each region, similar to a default VPC
 }
 
 resource "google_compute_subnetwork" "my_subnet" {
-  project       = "<your project here>"
   name          = "my-subnet"
+  project       = "<your project here>"
+  # project       = var.project_id
   ip_cidr_range = "192.168.0.0/24"
   region        = "us-central1"
   network       = google_compute_network.my_vpc.id
 }
 
 resource "google_compute_instance" "my_www_vm_1" {
-  project      = "my-project"
   name         = "my-www-vm-1"
+  project      = "<your project here>"
+  # project      = var.project_id
   machine_type = "e2-micro"
   zone         = "us-central1-b"
   tags = ["www"]
@@ -55,9 +62,18 @@ resource "google_compute_instance" "my_www_vm_1" {
   service_account {
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
-  # allow_stopping_for_update = true
+  
+  metadata = {
+    enable-oslogin = "TRUE"
+  }
+
+  # uncomment below to stop the virtual machine (VM) instance if an update to the
+  # instance requires it to be stopped.
+  # allow_stopping_for_update = true  
 }
 
+# manage project-wide metadata for Google Compute Engine
+# the setting below will enable OS Login project-wide for GCE Instances 
 # resource "google_compute_project_metadata" "default" {
 #  metadata = {
 #    enable-oslogin = "TRUE"
@@ -65,7 +81,8 @@ resource "google_compute_instance" "my_www_vm_1" {
 
 resource "google_compute_firewall" "allow_www" {
   name          = "allow-www"
-  project       = "qwiklabs-gcp-01-611bc819173a"
+  project       = "<your project here>"
+  # project       = var.project_id
   network       = google_compute_network.my_vpc.id
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["www"]
@@ -77,11 +94,12 @@ resource "google_compute_firewall" "allow_www" {
 
 resource "google_compute_firewall" "allow_iap" {
   name          = "allow-iap"
-  project       = "qwiklabs-gcp-01-611bc819173a"
+  project       = "<your project here>"
+  # project       = var.project_id
   network       = google_compute_network.my_vpc.id
   source_ranges = ["35.235.240.0/20"]
   allow {
-    protocol = "tcp"
-    ports    = ["22"]
+    protocol    = "tcp"
+    ports       = ["22"]
   }
 }
